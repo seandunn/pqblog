d600b0a8e4378bb5a3e238ecf4331ff77cd67afd
Sean Dunn <seanpeterdunn@gmail.com>
2015-05-04 12:53:25 +0100

    Store and display articles

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -6,4 +6,16 @@ class Admin::ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+
+  def create
+    article = Article.new(article_params)
+    article.save
+    redirect_to article
+  end
+  
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+  private :article_params
+
 end
