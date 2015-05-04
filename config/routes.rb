1223a46739fc8f0a4adb59cd20a846b79e923296
Sean Dunn <seanpeterdunn@gmail.com>
2015-05-04 15:17:31 +0100

    Add subscription routes

--- config/routes.rb
+++ config/routes.rb
@@ -61,5 +61,6 @@ Rails.application.routes.draw do
 
   resources :articles, only: [:index, :show] do
     resources :comments, only: :create
+    resources :subscriptions, only: [:create, :destroy]
   end
 end
