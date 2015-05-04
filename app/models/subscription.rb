c378f27efd8a0b1838871e7b3cb015bde92d3c10
Sean Dunn <seanpeterdunn@gmail.com>
2015-05-04 17:27:29 +0100

    Add subscription form to layout

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,2 +1,12 @@
-class Subscription < ActiveRecord::Base
+class EmailValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+      record.errors[attribute] << (options[:message] || "is not an email")
+    end
+  end
 end
+
+class Subscription < ActiveRecord::Base
+  validates :email, presence: true, uniqueness: true, email: true
+end
+
