2201fb731e865b63c34f40b5427ecb01a3a36c2b
Sean Dunn <seanpeterdunn@gmail.com>
2015-05-04 15:54:09 +0100

    Add a subscription model

---
+++ db/migrate/20150504143313_create_subscriptions.rb
@@ -0,0 +1,10 @@
+class CreateSubscriptions < ActiveRecord::Migration
+  def change
+    create_table :subscriptions do |t|
+      t.string :screen_name
+      t.string :email
+
+      t.timestamps null: false
+    end
+  end
+end
