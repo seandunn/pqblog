e461012969b236a93e1335083d0ea1ebb87ccd99
Sean Dunn <seanpeterdunn@gmail.com>
2015-05-07 19:37:16 +0100

    Set perform_deliveries to false in test

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
   #   location: '/usr/sbin/sendmail',
   #   arguments: '-i -t'
   # }
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_deliveries = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_options = {from: 'no-reply@example.com'}
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
