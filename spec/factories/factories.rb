27b8147e33d4181de8709c07da31c73b36f31f2c
Sean Dunn <seanpeterdunn@gmail.com>
2015-05-07 19:15:57 +0100

    Add unsubscribe test

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -4,7 +4,7 @@ FactoryGirl.define do
     body  'TEST_BODY'
   end
 
-  sequence :email do 
+  sequence :email do
     # Using a normal sequence fails non-db:prepared runs e.g. under guard.
     "test#{Time.now.to_i}@example.com"
   end
